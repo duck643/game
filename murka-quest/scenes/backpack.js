d548fdaa41dd85878f2173e94974459038447869
duck643 <mancurovae6@gmail.com>
2026-03-10 22:17:30 +0300

    Update backpack.js

--- murka-quest/scenes/backpack.js
+++ murka-quest/scenes/backpack.js
@@ -1,7 +1,6 @@
 const backpackScenes = {
-    // === СБОР РЮКЗАКА ===
     backpack_intro: {
-        name: "Мурка",
+        name: "Аноним",
         text: (state) => `Так вот, "${state.playerName}", первым твоим заданием будет собрать рюкзак, мы кое-куда пойдём. Что ты с собой возьмёшь из еды?`,
         choices: [
             { text: "а) Банка тушёнки", next: 'backpack_tushonka' },
@@ -12,34 +11,26 @@ const backpackScenes = {
     },
     
     backpack_tushonka: {
-        name: "Мурка",
-        text: (state) => `А зачем она тебе? Нам идти-то минут 30. ${state.playerName}, ты реально из космоса? Ладно, пошли, идём в парк.`,
-        choices: [
-            { text: "Идти в парк", next: 'scooters_choice' }
-        ]
+        name: "Аноним",
+        text: (state) => `А зачем она тебе? Нам идти-то минут 30.\n${state.playerName}, ты реально из космоса?`,
+        choices: [{ text: "Ладно, идём в парк", next: 'scooters_choice' }]
     },
     
     backpack_syr: {
-        name: "Мурка",
-        text: "Одобряю. Ладно, пошли, идём в парк.",
-        choices: [
-            { text: "Идти в парк", next: 'scooters_choice' }
-        ]
+        name: "Аноним",
+        text: "Одобряю.",
+        choices: [{ text: "Ладно, идём в парк", next: 'scooters_choice' }]
     },
     
     backpack_macaroni: {
-        name: "Мурка",
-        text: "А варить-то их где будешь? Ладно, пошли, идём в парк.",
-        choices: [
-            { text: "Идти в парк", next: 'scooters_choice' }
-        ]
+        name: "Аноним",
+        text: "А варить-то их где будешь?",
+        choices: [{ text: "Ладно, идём в парк", next: 'scooters_choice' }]
     },
     
     backpack_orehki: {
-        name: "Мурка",
-        text: "Одобряю. Ладно, пошли, идём в парк.",
-        choices: [
-            { text: "Идти в парк", next: 'scooters_choice' }
-        ]
+        name: "Аноним",
+        text: "Одобряю.",
+        choices: [{ text: "Ладно, идём в парк", next: 'scooters_choice' }]
     }
 };
